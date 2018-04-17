018cf22f3272a285af197a6531e4ba33adfbfd90
deadlyhallows <awanti2019@gmail.com>
2018-04-17 22:28:26 +0530

    added crud views and url,static files setup,added bootstrap.

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+from  __future__ import unicode_literals
 
+from django.db import models
+from django.core.urlresolvers import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length= 250)
@@ -7,8 +9,15 @@ class Post(models.Model):
     timestamp = models.DateField(auto_now=False,auto_now_add=True)
     updated = models.DateField(auto_now=True,auto_now_add=False)
 
+    def __unicode__(self):
+        return self.title
 
-def __self__(self):
-    return self.title
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('posts:detail',kwargs={"id":self.id})
+
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.http import HttpResponse ,HttpResponseRedirect
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Post
+from django.contrib import messages
+from .forms import PostForm
 
 # Create your views here.
+def post_create(request):
+    form = PostForm(request.POST or None)
+
+    if form.is_valid()==True:
+        instance=form.save(commit=False)
+        instance.save()
+        messages.success(request,"created successfully")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request,"not created")
+    context = {"form": form}
+
+    return render(request,"post_create.html",context)
+
+
+def post_list(request):
+    queryset = Post.objects.all()
+    context= {
+        "object_list":queryset,
+        "title": "title"
+
+    }
+
+    return render(request, 'post_list.html',context)
+
+
+def post_detail(request,id=None):
+    instance = get_object_or_404(Post,id=id)
+    context={
+         "objs":instance,
+         "title":instance.title
+    }
+    return render(request,'post_detail.html',context)
+
+
+def post_update(request,id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None,instance=instance)
+
+    if form.is_valid() == True:
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request,"Item Saved",)
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        "objs": instance,
+        "title": instance.title,
+        "form": form
+    }
+    return render(request, "post_create.html", context)
+
+
+def post_delete(request,id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    return  redirect("posts:list")
+
+
