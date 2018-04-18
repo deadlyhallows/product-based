10b552a395da57fa3a89893599d14ca2d3f7f994
deadlyhallows <awanti2019@gmail.com>
2018-04-18 15:22:49 +0530

    ordering and pagination applied.

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse ,HttpResponseRedirect
 from django.shortcuts import render,get_object_or_404,redirect
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 from .models import Post
 from django.contrib import messages
 from .forms import PostForm
@@ -21,15 +22,22 @@ def post_create(request):
 
 
 def post_list(request):
-    queryset = Post.objects.all()
-    context= {
-        "object_list":queryset,
-        "title": "LIST"
-
+    queryset_list = Post.objects.all()#.order_by("-timestamp")
+    paginator = Paginator(queryset_list, 5)
+    page_change_var = 'page' #change=request
+    page = request.GET.get(page_change_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
+    context = {
+        "object_list": queryset,
+        "title": "LIST",
+        "page_change_var":page_change_var
     }
-
-    return render(request, 'post_list.html',context)
-
+    return render(request, 'post_list.html', context)
 
 def post_detail(request,id=None):
     instance = get_object_or_404(Post,id=id)
