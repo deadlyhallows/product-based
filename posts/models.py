963a4872e5ac32db282f4322cc6fb73b5cc41b0f
deadlyhallows <awanti2019@gmail.com>
2018-04-18 22:58:04 +0530

    adding imagefield with height and width field

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,12 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length= 250)
-    image = models.FileField(null=True,blank=True)
+    image = models.ImageField(
+        null=True,blank=True,
+        height_field="height_field",
+        width_field="width_field")
+    height_field=models.IntegerField(default=0)
+    width_field=models.IntegerField(default=0)
     content = models.TextField()
     timestamp = models.DateField(auto_now=False,auto_now_add=True)
     updated = models.DateField(auto_now=True,auto_now_add=False)
