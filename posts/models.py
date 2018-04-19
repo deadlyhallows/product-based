12caaba9158edb40c6031c18b2583fb3e0fcb3a9
deadlyhallows <awanti2019@gmail.com>
2018-04-19 23:11:14 +0530

    upload location in %s/%s.%s format.and added null and blank feature in height and width field.

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,8 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 
 def upload_location(instance,filename):
-    return "%s/%s" %(instance.id,filename)
+    filebase,extension=filename.split(".")
+    return "%s/%s.%s" %(instance,instance.id,extension)
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
@@ -12,8 +13,8 @@ class Post(models.Model):
         null=True,blank=True,
         height_field="height_field",
         width_field="width_field")
-    height_field=models.IntegerField(default=0)
-    width_field=models.IntegerField(default=0)
+    height_field=models.IntegerField(default=None,null=True,blank=True)
+    width_field=models.IntegerField(default=None,null=True,blank=True)
     content = models.TextField()
     timestamp = models.DateField(auto_now=False,auto_now_add=True)
     updated = models.DateField(auto_now=True,auto_now_add=False)
