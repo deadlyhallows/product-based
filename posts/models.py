0a9764c65c4a5a73d6362b67f2fc26eee54c689b
deadlyhallows <awanti2019@gmail.com>
2018-04-19 22:28:26 +0530

    upload location in %s/%s format

--- posts/models.py
+++ posts/models.py
@@ -3,11 +3,12 @@ from django.db import models
 from django.core.urlresolvers import reverse
 # Create your models here.
 
-
+def upload_location(instance,filename):
+    return "%s/%s" %(instance.id,filename)
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    image = models.ImageField(
+    image = models.ImageField(upload_to=upload_location,
         null=True,blank=True,
         height_field="height_field",
         width_field="width_field")
