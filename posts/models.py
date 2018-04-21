1ab4c431ea6608cc71c1653fa866ed04ec51088b
deadlyhallows <awanti2019@gmail.com>
2018-04-21 17:18:25 +0530

    feat:added slug but need modifications fro repetations.

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,8 @@
 from  __future__ import unicode_literals
-from django.db import models
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 # Create your models here.
 
 def upload_location(instance,filename):
@@ -9,11 +11,13 @@ def upload_location(instance,filename):
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location,
         null=True,blank=True,
         height_field="height_field",
         width_field="width_field")
     height_field=models.IntegerField(default=None,null=True,blank=True)
+
     width_field=models.IntegerField(default=None,null=True,blank=True)
     content = models.TextField()
     timestamp = models.DateField(auto_now=False,auto_now_add=True)
@@ -31,4 +35,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-timestamp','-updated']#important
+
+def pre_save_post_receiver(sender,instance,*args,**kwargs):
+    slug =slugify(instance.title)
+    exist =Post.objects.filter(slug=slug).exists()
+    if exist:
+        slug = "%s-%s" %(slug,instance.id)
+    instance.slug = slug
+
+
+pre_save.connect(pre_save_post_receiver,sender=Post)
 
