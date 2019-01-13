f8facce5e14d11a85ab04f874febfe56c438e2e7
deadlyhallows <awanti2019@gmail.com>
2019-01-13 21:26:28 +0530

    Add:created user api

--- product/urls.py
+++ product/urls.py
@@ -17,18 +17,23 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include("posts.urls", namespace='posts')),
+    url(r'^register/', register_view, name='register'),
+    url(r'^login/', login_view, name='login'),
+    url(r'^logout/', logout_view, name='logout'),
+    url(r'^', include("posts.urls", namespace='posts')),
+    url(r'^api/users/', include('accounts.api.urls', namespace='users-api')),
     url(r'^api/posts/', include('posts.api.urls', namespace='posts-api')),
-    #url(r'^posts/$', "<appname>.views.<function_name>"),
+    # url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
-
-#if settings.DEBUG is False:
- #   urlpatterns += [url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}), ]
+# if settings.DEBUG is False:
+#   urlpatterns += [url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}), ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
